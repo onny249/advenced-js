16cddd0eda3baddb10125cb036bdeac968a332d8
onny249 <khadizaonny12345@gmail.com>
2023-07-16 16:23:11 +0600

    advanced js

---
+++ truthy&FalsyValue.js
@@ -0,0 +1,13 @@
+// const name = " ";
+// false hobe j j somoy ta holo: 1.value 0 hole..2. Null hole..3.variable declare kore value nah dile..4. empty string hole "" 5. Nan(not a number) hole..baki shob true hobe..like:
+//true: '0', ' ', [],{}
+let name = {};
+console.log(name);
+
+if (name) {
+    console.log('condition is true');
+}
+
+else{
+    console.log('condition is false');
+}
