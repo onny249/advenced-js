5976c55abfc9f809e43dad7fd8f5e2c32179380e
onny249 <khadizaonny12345@gmail.com>
2023-07-16 17:45:09 +0600

    all intervire question

---
+++ NullVsUndefined.js
@@ -0,0 +1,20 @@
+// variable declaye korer pore jodi value pass nah kora hoy.
+
+let value;
+console.log(value);
+
+// function return nah korleo output undefined hoy.
+
+function add(num1, num2){
+    console.log(num1+num2);
+}
+
+const result = add(12,20);
+console.log(result);//undefined output ashbe.
+
+//object e value nah thaka. 
+
+const Info = {name:'onny chowdhury', phone: '0124383773', address:'reter' }
+console.log(Info.university);
+
+
